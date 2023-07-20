38190b4eeb0752d17ffe0516d478e9f0f7f4b6dd
Alejandra <20201923@aloe.ulima.edu.pe>
2023-07-20 16:28:05 -0500

    ayuda 23

--- index.js
+++ index.js
@@ -25,7 +25,13 @@ const port = process.env.PORT || 3001;
     origin: "*",
     optionsSuccessStatus: 200
 }))*/
-app.use(cors());
+
+const corsOptions ={
+    origin:'*', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+  }
+app.use(cors(corsOptions));
 
 app.use(express.json())
 
