92dbbd861fe15acc23e4a30f856d9b1917e8a9ac
Alejandra <20201923@aloe.ulima.edu.pe>
2023-07-20 16:36:18 -0500

    ayuda 25

--- index.js
+++ index.js
@@ -23,17 +23,7 @@ import { capitalizeFirstLetter } from "./utils/funcionesBienUtiles.js";
 const app = express()
 const port = process.env.PORT || 3001;
 
-/*app.use(cors({
-    origin: "*",
-    optionsSuccessStatus: 200
-}))*/
-
-const corsOptions ={
-    origin:'*', 
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200
-  }
-app.use(cors(corsOptions));
+app.use(cors());
 
 app.use(express.json())
 
