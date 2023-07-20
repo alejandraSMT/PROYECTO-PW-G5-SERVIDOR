2987e0e88b9eaba9b4a0fdae407dd9daab9656f6
Alejandra <20201923@aloe.ulima.edu.pe>
2023-07-20 14:45:35 -0500

    ayuda 12

--- index.js
+++ index.js
@@ -1498,48 +1498,70 @@ app.get("/usuarios/:parametro", async (req, res) => {
 // ------------------------------- CRISTOPHER --------------------------
 
 
-app.get("/horarios/:diaSemana/:horaInicio/:horaFin/:enlaceSesion",
-    async (req, res) => {
-        try {
-            const diaSemana = req.params.diaSemana;
-            const horaInicio = req.params.horaInicio;
-            const horaFin = req.params.horaFin;
-            const enlaceSesion = req.params.enlaceSesion;
-            const horario = await Horario.create({
-                diaSemana: diaSemana,
-                horaInicio: horaInicio,
-                horaFin: horaFin,
-                enlaceSesion: enlaceSesion,
-            });
-            res.send(horario);
-            console.log(horario);
-        } catch (error) {
-            res.json({ error: "No se pudo agregar el horario" });
-        }
+app.get("/horarios", async (req, res) => {
+    try {
+      const horarios = await Horario.findAll({
+        include: [Profesor],
+      });
+      const horariosData = horarios.map((horario) => ({
+        id: horario.id,
+        diaSemana: horario.diaSemana,
+        horaInicio: horario.horaInicio,
+        horaFin: horario.horaFin,
+        enlaceSesion: horario.enlaceSesion,
+      }));
+  
+      res.send(horariosData);
+      console.log(horariosData);
+    } catch (error) {
+      res.status(500).json({ error: "Failed to retrieve horarios" });
     }
-);
-
-//Al presionar el boton X de la columna de Horarios, se debe enviar esta peticion
-app.get("/remover-horario/:idHorario",
+  });
+  
+  //Endpoint para agregar un nuevo Horario.
+  //Apartir de este se puede aramar para eliminar uno de la base de datos
+  
+  app.get("/horarios/:diaSemana/:horaInicio/:horaFin/:enlaceSesion",
     async (req, res) => {
-        try {
-            const id = req.params.idHorario;
-            const deletedRows = await Horario.destroy({
-                where: {
-                    id: id,
-                },
-            });
-            if (deletedRows > 0) {
-                res.send("Horario Eliminado")
-                res.status(200).json({ message: 'Horario deleted successfully' });
-            } else {
-                res.status(404).json({ message: 'Horario not found' });
-            }
-        } catch (error) {
-            res.status(500).json({ error: 'Failed to delete the horario' });
-        }
+      try {
+        const diaSemana = req.params.diaSemana;
+        const horaInicio = req.params.horaInicio;
+        const horaFin = req.params.horaFin;
+        const enlaceSesion = req.params.enlaceSesion;
+        const horario = await Horario.create({
+          diaSemana: diaSemana,
+          horaInicio: horaInicio,
+          horaFin: horaFin,
+          enlaceSesion: enlaceSesion,
+        });
+        res.send(horario);
+        console.log(horario);
+      } catch (error) {
+        res.json({ error: "No se pudo agregar el horario" });
+      }
     }
-);
+  );
+  
+  //Al presionar el boton X de la columna de Horarios, se debe enviar esta peticion
+  app.get("/remover-horario/:idHorario",
+    async (req, res) => {
+      try {
+        const id = req.params.idHorario;
+        const deletedRows = await Horario.destroy({
+          where: {
+            id: id,
+          },
+        });
+        if (deletedRows > 0) {
+          res.status(200).json({ message: 'Horario deleted successfully' });
+        } else {
+          res.status(404).json({ message: 'Horario not found' });
+        }
+      } catch (error) {
+        res.status(500).json({ error: 'Failed to delete the horario' });
+      }
+    }
+  );
 
 app.get("/", function (req, res) {
     res.send("Se conectó correctamente");
